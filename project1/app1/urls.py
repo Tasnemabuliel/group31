78c7ea1ce964bd476b2daaddbc07ad7f8a732430
Mohamad2064 <65393790+Mohamad2064@users.noreply.github.com>
2022-12-29 09:30:39 +0200

    Add files via upload

--- project1/app1/urls.py
+++ project1/app1/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from app1 import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('signup',views.signup,name='signup'),
-    path('signin',views.signup,name='signin'),
-    path('signout',views.signup,name='signout'),
+    path('signin',views.signin,name='signin'),
+    path('signout',views.signout,name='signout'),
     path('HomePage',views.HomePage,name='HomePage'),
+    path('Profile',views.profile,name='profile'),
+    path('settings',views.settings,name='settings'),
+
     ]
-   
--- project1/app1/views.py
+++ project1/app1/views.py
@@ -2,21 +2,54 @@ from contextlib import _RedirectStream
 from email import message
 from django.shortcuts import redirect,render
 from django.http import HttpResponse
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout
 from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
+#from models import *
+from .forms import createUserForm
+
 def home(request):
     return render(request,'app1/index.html')
 
 
 def signup(request):
-    
-    return render(request,'app1/signup.html')
+    form=UserCreationForm()
+    if request.method=="POST":
+         form=createUserForm(request.POST)
+         if form.is_valid():
+            form.save()
+            user=form.cleaned_data.get('userame')
+            messages.success(request,'Account was created for'+user)
+            return redirect('signin')
+
+    context={'form':form}
+    return render(request,'app1/signup.html',context)
 
 def signin(request):
+    if(request.method=="POST"):
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('HomePage')
+
+
     return render(request,'app1/signin.html')
 
 def signout(request):
-    pass
+    logout(request)
+    return redirect('home')
+
 def HomePage(request):
-    return render(request,'app1/HomePage.html')
+    return render(request,'app1/HomePage.html')  
+
+
+def profile(request):
+    return render(request,'app1/profile.html')
+
+
+def settings(request):
+    return render(request,'app1/settings.html')
